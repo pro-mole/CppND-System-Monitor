14666545f9a094f81082b9adc09bc4e057654cc4
Bruno Guedes <guedesav@brazmogu.com.br>
2019-08-31 00:34:57 -0300

    Implement Elapsed Time Formatter

--- src/format.cpp
+++ src/format.cpp
@@ -4,8 +4,43 @@
 
 using std::string;
 
-// TODO: Complete this helper function
+const int DAY_IN_SECONDS = 24 * 60 * 60;
+const int HOUR_IN_SECONDS = 60 * 60;
+
+// DONE: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  std::string timeString = "";
+
+  long remaining_seconds = seconds;
+
+  // [IMPROVE] Add Days to the format
+  /*
+  int days = seconds / DAY_IN_SECONDS;
+
+  if (days > 0) {
+      timeString += std::to_string(days);
+      if (days > 1) {
+          timeString += " days, ";
+      }
+      else {
+          timeString += " day, ";
+      }
+  }
+
+  remaining_seconds = remaining_seconds % DAY_IN_SECONDS;
+  */
+  int hours = remaining_seconds / HOUR_IN_SECONDS;
+  timeString += std::to_string(hours) + ":";
+
+  remaining_seconds = remaining_seconds % HOUR_IN_SECONDS;
+  int minutes = remaining_seconds / 60;
+  timeString += std::to_string(minutes) + ":";
+
+  remaining_seconds = remaining_seconds % 60;
+  timeString += std::to_string(remaining_seconds);
+
+  return timeString;
+}
