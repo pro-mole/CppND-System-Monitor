36099bd7dea7481c114ba4a6a88fb77363730ae8
Bruno Guedes <guedesav@brazmogu.com.br>
2019-08-31 00:11:21 -0300

    Add Parser calls to show info on the monitor

--- src/system.cpp
+++ src/system.cpp
@@ -20,22 +20,20 @@ Processor& System::Cpu() { return cpu_; }
 // TODO: Return a container composed of the system's processes
 vector<Process>& System::Processes() { return processes_; }
 
-// TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+// DOING: Return the system's kernel identifier (string)
+std::string System::Kernel() { return LinuxParser::Kernel(); }
 
 // DONE: Return the system's memory utilization
-float System::MemoryUtilization() { 
-    return LinuxParser::MemoryUtilization();
-}
+float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+// DOING: Return the operating system name
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
-// TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return 0; }
+// DOING: Return the number of processes actively running on the system
+int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
 
-// TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
+// DOING: Return the total number of processes on the system
+int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
 
-// TODO: Return the number of seconds since the system started running
-long int System::UpTime() { return 0; }
+// DOING: Return the number of seconds since the system started running
+long int System::UpTime() { return LinuxParser::UpTime(); }
